f3ae56f7bd9f98e06daae7a05cba6b572a236531
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-06-16 23:48:09 -0700

    styling done

--- src/App.js
+++ src/App.js
@@ -42,6 +42,15 @@ function App() {
             <div className="location">New York City, US</div>
             <div className="date">{dateBuilder(new Date)}</div>
 
+            <div className="weather-box">
+              <div className="temp">
+                15*c
+              </div>
+              <div className="weather">
+                Sunny
+              </div>
+            </div>
+
         </div>
       </main>
     </div>
